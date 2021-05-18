b03722f7e836f67765a5cce1fcd6753875203077
kristi4n <57930350+kristi4n@users.noreply.github.com>
2021-05-18 23:05:53 +0800

    Login Form + Penyesuaian
    - Menambah Login Form
    - Menyesuaikan Ukuran menjadi 813;448 (FormTransaksi, FromLaporanTransaksi)

---
+++ Berkat Mandiri/FormLogin.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace Berkat_Mandiri
+{
+    partial class FormLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Pnl_Bg = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Pnl_LoginForm = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Tb_User = new System.Windows.Forms.TextBox();
+            this.Tb_Pass = new System.Windows.Forms.TextBox();
+            this.Btn_LoginAdmin = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Btn_LoginUser = new System.Windows.Forms.Button();
+            this.Pnl_Bg.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.Pnl_LoginForm.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Pnl_Bg
+            // 
+            this.Pnl_Bg.Controls.Add(this.Pnl_LoginForm);
+            this.Pnl_Bg.Controls.Add(this.panel1);
+            this.Pnl_Bg.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Pnl_Bg.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Pnl_Bg.Location = new System.Drawing.Point(15, 15);
+            this.Pnl_Bg.Name = "Pnl_Bg";
+            this.Pnl_Bg.Size = new System.Drawing.Size(770, 420);
+            this.Pnl_Bg.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(770, 49);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Nirmala UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(157)))), ((int)(((byte)(161)))), ((int)(((byte)(172)))));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(770, 49);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "BERKAT MANDIRI";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Pnl_LoginForm
+            // 
+            this.Pnl_LoginForm.Controls.Add(this.tableLayoutPanel1);
+            this.Pnl_LoginForm.Location = new System.Drawing.Point(73, 93);
+            this.Pnl_LoginForm.Name = "Pnl_LoginForm";
+            this.Pnl_LoginForm.Size = new System.Drawing.Size(573, 235);
+            this.Pnl_LoginForm.TabIndex = 2;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 36.82373F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 63.17627F));
+            this.tableLayoutPanel1.Controls.Add(this.Btn_LoginUser, 0, 5);
+            this.tableLayoutPanel1.Controls.Add(this.label5, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.Tb_Pass, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.Tb_User, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.Btn_LoginAdmin, 1, 3);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 6;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(573, 235);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label4.Font = new System.Drawing.Font("Nirmala UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(157)))), ((int)(((byte)(161)))), ((int)(((byte)(172)))));
+            this.label4.Location = new System.Drawing.Point(3, 78);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(204, 39);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Password";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label3
+            // 
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Font = new System.Drawing.Font("Nirmala UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(157)))), ((int)(((byte)(161)))), ((int)(((byte)(172)))));
+            this.label3.Location = new System.Drawing.Point(3, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(204, 39);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Username";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.label2, 2);
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Font = new System.Drawing.Font("Nirmala UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(157)))), ((int)(((byte)(161)))), ((int)(((byte)(172)))));
+            this.label2.Location = new System.Drawing.Point(3, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(567, 39);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Login Admin";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Tb_User
+            // 
+            this.Tb_User.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Tb_User.Location = new System.Drawing.Point(213, 42);
+            this.Tb_User.MaxLength = 20;
+            this.Tb_User.Name = "Tb_User";
+            this.Tb_User.Size = new System.Drawing.Size(357, 32);
+            this.Tb_User.TabIndex = 4;
+            // 
+            // Tb_Pass
+            // 
+            this.Tb_Pass.Location = new System.Drawing.Point(213, 81);
+            this.Tb_Pass.MaxLength = 16;
+            this.Tb_Pass.Name = "Tb_Pass";
+            this.Tb_Pass.PasswordChar = '*';
+            this.Tb_Pass.Size = new System.Drawing.Size(357, 32);
+            this.Tb_Pass.TabIndex = 5;
+            // 
+            // Btn_LoginAdmin
+            // 
+            this.Btn_LoginAdmin.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Btn_LoginAdmin.Location = new System.Drawing.Point(250, 119);
+            this.Btn_LoginAdmin.Margin = new System.Windows.Forms.Padding(40, 2, 40, 2);
+            this.Btn_LoginAdmin.Name = "Btn_LoginAdmin";
+            this.Btn_LoginAdmin.Size = new System.Drawing.Size(283, 35);
+            this.Btn_LoginAdmin.TabIndex = 6;
+            this.Btn_LoginAdmin.Text = "Login";
+            this.Btn_LoginAdmin.UseVisualStyleBackColor = true;
+            this.Btn_LoginAdmin.Click += new System.EventHandler(this.Btn_LoginAdmin_Click);
+            // 
+            // label5
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.label5, 2);
+            this.label5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label5.Font = new System.Drawing.Font("Nirmala UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(157)))), ((int)(((byte)(161)))), ((int)(((byte)(172)))));
+            this.label5.Location = new System.Drawing.Point(3, 156);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(567, 39);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Or";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Btn_LoginUser
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.Btn_LoginUser, 2);
+            this.Btn_LoginUser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Btn_LoginUser.Location = new System.Drawing.Point(40, 197);
+            this.Btn_LoginUser.Margin = new System.Windows.Forms.Padding(40, 2, 40, 2);
+            this.Btn_LoginUser.Name = "Btn_LoginUser";
+            this.Btn_LoginUser.Size = new System.Drawing.Size(493, 36);
+            this.Btn_LoginUser.TabIndex = 8;
+            this.Btn_LoginUser.Text = "Login As User";
+            this.Btn_LoginUser.UseVisualStyleBackColor = true;
+            this.Btn_LoginUser.Click += new System.EventHandler(this.Btn_LoginUser_Click);
+            // 
+            // FormLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(53)))), ((int)(((byte)(70)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Pnl_Bg);
+            this.Name = "FormLogin";
+            this.Padding = new System.Windows.Forms.Padding(15);
+            this.Text = "FormLogin";
+            this.Pnl_Bg.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.Pnl_LoginForm.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Pnl_Bg;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel Pnl_LoginForm;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button Btn_LoginUser;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox Tb_Pass;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Tb_User;
+        private System.Windows.Forms.Button Btn_LoginAdmin;
+    }
+}
---
+++ Berkat Mandiri/FormLogin.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Berkat_Mandiri
+{
+    public partial class FormLogin : Form
+    {
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_LoginAdmin_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DbConnect.connection.Open();
+                MySqlCommand Com = new MySqlCommand()
+                {
+                    CommandText = "login_admin",
+                    CommandType = CommandType.StoredProcedure,
+                    Connection = DbConnect.connection
+                };
+                DataTable dt = new DataTable();
+                MySqlParameter[] pam = new MySqlParameter[2];
+                pam[0] = new MySqlParameter("nam", MySqlDbType.VarChar) {Value = Tb_User.Text };
+                pam[1] = new MySqlParameter("pas", MySqlDbType.VarChar) { Value = Tb_User.Text };
+                Com.Parameters.AddRange(pam);
+                MySqlDataAdapter adap = new MySqlDataAdapter(Com);
+                adap.Fill(dt);
+                int HasilNilai = Convert.ToInt32(dt.Rows[0][0].ToString());
+                if (HasilNilai == 1)
+                {
+                    //Untuk Procedure / funtion masuk ke dashboard sebagai ADMIN
+                    Masuk_Sebagai_Admin();
+                }
+                else
+                {
+                    MessageBox.Show("Username atau Password yang dimasukkan salah. Silahkan mencoba lagi.", "Login Gagal", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+                DbConnect.connection.Close();
+            }
+            catch (Exception ex)
+            {
+                DbConnect.connection.Close();
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Btn_LoginUser_Click(object sender, EventArgs e)
+        {
+            Masuk_Sebagai_User_Biasa();
+        }
+        private void Masuk_Sebagai_Admin()
+        {
+            //Untuk Procedure / funtion masuk ke dashboard sebagai ADMIN
+            MessageBox.Show("Masuk Sebagai Admin Berhasil", "Admin Login", MessageBoxButtons.OK, MessageBoxIcon.Information); // Ini Boleh Dihapus / dicomment
+        }
+        private void Masuk_Sebagai_User_Biasa()
+        {
+            //Untuk Procedure / funtion masuk ke dashboard sebagai User biasa yang bisa melihat stok dan memasukan kasir penjualan
+            MessageBox.Show("Masuk Sebagai User Biasa Berhasil", "User Biasa Login", MessageBoxButtons.OK, MessageBoxIcon.Information); // Ini Boleh Dihapus / dicomment
+        }
+    }
+}
