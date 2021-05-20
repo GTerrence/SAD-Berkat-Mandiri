4ee70b806aa56e872e1f8c265c6289810d01246f
aldisatria12 <56900446+aldisatria12@users.noreply.github.com>
2021-05-20 12:01:40 +0700

    update fix mysql

--- Berkat Mandiri/Program.cs
+++ Berkat Mandiri/Program.cs
@@ -16,12 +16,7 @@ namespace Berkat_Mandiri
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-<<<<<<< HEAD
             Application.Run(new FormBase());
-=======
-            //Application.Run(new FormBase());
-            Application.Run(new FormLogin());
->>>>>>> branch-kristian
         }
     }
 }
