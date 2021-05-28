de2d4c6f82657cf49c19c497acfab3bda65feeca
kristi4n <57930350+kristi4n@users.noreply.github.com>
2021-05-28 15:50:29 +0800

    Pembaharuan di Transaksi Penjualan
    - Textbox Pelanggan di ganti jadi Combobox
    - Sudah bisa print Nota

--- Berkat Mandiri/Form_Dashboard.Designer.cs
+++ Berkat Mandiri/Form_Dashboard.Designer.cs
@@ -29,13 +29,13 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.lblJumlah = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.btnKasir = new System.Windows.Forms.Button();
+            this.lblJumlah = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.btnStockV = new System.Windows.Forms.Button();
+            this.btnKasir = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
@@ -51,26 +51,15 @@
             this.panel1.Size = new System.Drawing.Size(187, 117);
             this.panel1.TabIndex = 0;
             // 
-            // panel2
+            // label3
             // 
-            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(196)))), ((int)(((byte)(196)))), ((int)(((byte)(196)))));
-            this.panel2.Controls.Add(this.btnStockV);
-            this.panel2.Controls.Add(this.btnKasir);
-            this.panel2.Controls.Add(this.label4);
-            this.panel2.Location = new System.Drawing.Point(490, 48);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(238, 117);
-            this.panel2.TabIndex = 1;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Britannic Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(40, 17);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(82, 23);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Piutang";
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Britannic Bold", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(84, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(25, 32);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "!";
             // 
             // lblJumlah
             // 
@@ -83,34 +72,26 @@
             this.lblJumlah.TabIndex = 1;
             this.lblJumlah.Text = "S";
             // 
-            // label3
+            // label1
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Britannic Bold", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(84, 52);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(25, 32);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "!";
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Britannic Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(40, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Piutang";
             // 
-            // label4
+            // panel2
             // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Britannic Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(14, 17);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(130, 23);
-            this.label4.TabIndex = 0;
-            this.label4.Text = "Quick Access";
-            // 
-            // btnKasir
-            // 
-            this.btnKasir.Location = new System.Drawing.Point(18, 52);
-            this.btnKasir.Name = "btnKasir";
-            this.btnKasir.Size = new System.Drawing.Size(75, 23);
-            this.btnKasir.TabIndex = 1;
-            this.btnKasir.Text = "Kasir";
-            this.btnKasir.UseVisualStyleBackColor = true;
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(196)))), ((int)(((byte)(196)))), ((int)(((byte)(196)))));
+            this.panel2.Controls.Add(this.btnStockV);
+            this.panel2.Controls.Add(this.btnKasir);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Location = new System.Drawing.Point(490, 48);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(238, 117);
+            this.panel2.TabIndex = 1;
             // 
             // btnStockV
             // 
@@ -121,6 +102,25 @@
             this.btnStockV.Text = "Stok";
             this.btnStockV.UseVisualStyleBackColor = true;
             this.btnStockV.Click += new System.EventHandler(this.btnStockV_Click);
+            // 
+            // btnKasir
+            // 
+            this.btnKasir.Location = new System.Drawing.Point(18, 52);
+            this.btnKasir.Name = "btnKasir";
+            this.btnKasir.Size = new System.Drawing.Size(75, 23);
+            this.btnKasir.TabIndex = 1;
+            this.btnKasir.Text = "Kasir";
+            this.btnKasir.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Britannic Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(14, 17);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(130, 23);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Quick Access";
             // 
             // Form_Dashboard
             // 
